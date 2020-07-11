00156366f3a4d2e63fad7aac273534766f65ec57
ryancho <chowin21@gmail.com>
2020-07-11 15:34:04 +0900

    #010 Add-split the App into module

---
+++ src/app/training/training.module.ts
@@ -0,0 +1,32 @@
+//common
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MaterialModule} from "../material.module";
+
+//View
+import {TrainingComponent} from "./training.component";
+import {CurrentTrainingComponent} from "./current-training/current-training.component";
+import {NewTrainingComponent} from "./new-training/new-training.component";
+import {PastTrainingsComponent} from "./past-trainings/past-trainings.component";
+import {StopTrainingComponent} from "./current-training/stop-training.component";
+import {AngularFirestoreModule} from "angularfire2/firestore";
+
+@NgModule({
+    declarations: [
+        TrainingComponent,
+        CurrentTrainingComponent,
+        NewTrainingComponent,
+        PastTrainingsComponent,
+        StopTrainingComponent,
+        ],
+    imports: [
+        MaterialModule,FormsModule,CommonModule,FlexLayoutModule,AngularFirestoreModule,
+    ],
+    exports: [],
+    entryComponents: [StopTrainingComponent]
+})
+export class TrainingModule {
+
+}
